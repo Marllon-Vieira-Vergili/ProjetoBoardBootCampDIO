31ddee67b98566edb9668312cebefbc18a7efa34
Marllon-Vieira-Vergili <marllonvergili@gmail.comgit clone https://github.com/Marllon-Vieira-Vergili/chess-system-java.gitgit config --global user.email marllonvergili@gmail.com>
2025-08-18 10:49:34 -0300

    Primeiro commit, começando a organização do projeto

---
+++ src/main/java/com/marllon/vieira/BoardGerenciamentoTarefaDIO/BoardGerenciamentoTarefaDioApplication.java
@@ -0,0 +1,13 @@
+package com.marllon.vieira.BoardGerenciamentoTarefaDIO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardGerenciamentoTarefaDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardGerenciamentoTarefaDioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marllon/vieira/BoardGerenciamentoTarefaDIO/BoardGerenciamentoTarefaDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marllon.vieira.BoardGerenciamentoTarefaDIO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardGerenciamentoTarefaDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
